0b089c08bbdb9217b305d72cc962c2592c3c3225
Hamar Csaba <hamarcsabika@gmail.com>
2026-02-26 17:22:49 +0100

    Rejtélyes üzenet

---
+++ 03/20120815l.py
@@ -0,0 +1,28 @@
+# Rejtélyes üzenet
+
+def decode_message(szoveg):
+    abc    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    offset = "cdefghijklmnopqrstuvwxyzabCDEFGHIJKLMNOPQRSTUVWXYZAB"
+    erdemeny = ""
+    for char in szoveg:
+        if char in abc:
+            erdemeny += offset[abc.index(char)]
+        else:
+            erdemeny += char
+    return erdemeny
+
+def main():
+    encoded_message = """
+Cbcq Dgyk!
+
+Dmeybh kce cew yrwyg hmrylyaqmr:
+rylsjb kce y Nwrfml npmepykmxyqg lwcjtcr!
+
+Aqmimjjyi:
+
+Ynyb""".strip()
+    print(decode_message(encoded_message))
+    
+
+if __name__ == "__main__":
+    main()
