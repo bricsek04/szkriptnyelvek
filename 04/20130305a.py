6e53bdaf3f51ae4f7cf73a442c883df9b8458d61
Hamar Csaba <hamarcsabika@gmail.com>
2026-03-05 14:35:49 +0100

    xor

---
+++ 04/20130305a.py
@@ -0,0 +1,15 @@
+# XOR
+
+def xor(a, b):
+    if bool(a) == bool(b):
+        return False
+    else:
+        return True
+    
+def main():
+    str1 = "Python"
+    str2 = None
+    print(xor(str1, str2))
+
+if __name__ == "__main__":
+    main()
