9c2d57730a8e677b488c2e4516fc5d9d182dd427
Hamar Csaba <hamarcsabika@gmail.com>
2026-03-12 12:51:11 +0100

    Eltérő viselkedés szimbolikus linken keresztül

---
+++ 05/20130919a.py
@@ -0,0 +1,20 @@
+# Eltérő viselkedés szimbolikus linken keresztül
+
+# Szimbolikus link létrehozása:
+# Lépjünk bele abbak a könyvtárba ami tartalmazza a 20130919a.py fájlt
+# Windows: mklink z-a.py 20130919a.py
+# Linux: ln -s 20130919a.py z-a.py
+
+import sys
+
+def abc():
+    return('abcdefghijklmnopqrstuvwxyz')
+
+def main():
+    if sys.argv[0] == 'z-a.py':
+        print(abc()[::-1])
+    else:
+        print(abc())
+
+if __name__ == '__main__':
+    main()
