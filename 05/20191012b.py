0f158a28c71eb652581baa4541c495b49e8ba4a3
Hamar Csaba <hamarcsabika@gmail.com>
2026-03-12 13:22:09 +0100

    Münchausen-számok

---
+++ 05/20191012b.py
@@ -0,0 +1,29 @@
+# Münchausen-számok
+
+def münchausen_numbers(numbers):
+    """
+        Münchausen-számok keresése
+
+        Paraméter:
+            numbers (iterable): A számokat tartalmazó iterálható objektum
+
+        Visszatérési érték:
+            list: A Münchausen-számok listája
+    """
+    münchausen_numbers = []
+    su = 0
+    for i in numbers:
+        if i == 0:
+            münchausen_numbers.append(i)
+        for c in str(i):
+            su += int(c)**int(c)
+        if su == i:
+            münchausen_numbers.append(i)
+        su = 0
+    return münchausen_numbers
+
+def main():
+    print(münchausen_numbers(range(10001)))
+
+if __name__ == '__main__':
+    main()
