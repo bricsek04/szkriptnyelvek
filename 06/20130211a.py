c9a629f3caef2b13c5c5227a862f5166dbacde97
Hamar Csaba <hamarcsabika@gmail.com>
2026-03-19 16:38:06 +0100

    Anagramma v2

--- 06/20130211a.py
+++ 06/20130211a.py
@@ -18,10 +18,16 @@ def is_anagramma(szo1, szo2):
             dict_szo2[c] = 1
     return(dict_szo1 == dict_szo2)
 
+def is_anagramma_v2(szo1, szo2):
+    szo1 = sorted(list(normalize(szo1)))
+    szo2 = sorted(list(normalize(szo2)))
+    return szo1 == szo2
+
 
 def main():
     print(normalize("Clint Eastwood"))
     print(is_anagramma("listen", "silent"))
+    print(is_anagramma_v2("listen", "silnet"))
 
 if __name__ == "__main__":
     main()
