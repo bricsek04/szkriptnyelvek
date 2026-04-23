d08affaae5ea26b4043bd2ee7333a9fc19863f8b
Hamar Csaba <hamarcsabika@gmail.com>
2026-04-23 14:30:05 +0200

    Verem megvalósítása

---
+++ 11/20141125a.py
@@ -0,0 +1,49 @@
+# Verem és sor megvalósítása saját osztállyal
+
+class Verem:
+    def __init__(self):
+        self.verem = []
+    
+    def __str__(self):
+        return str(self.verem)
+
+    def ures(self):
+        if self.verem:
+            return False
+        else:
+            return True
+    
+    def betesz(self, ertek):
+        self.verem.append(ertek)
+
+    def kivesz(self):
+        if self.verem:
+            return self.verem.pop()
+        else:
+            return None
+
+    def meret(self):
+        return len(self.verem)
+
+
+
+def main():
+    v = Verem()      # üres verem létrehozása
+    print(v)         # [
+    print(v.ures())  # True
+    v.betesz(1)
+    v.betesz(4)
+    v.betesz(5)
+    print(v)         # [1 4 5
+    print(v.meret()) # 3
+    print(v.ures())  # False
+    x = v.kivesz()
+    print(x)         # 5
+    print(v)         # [1 4
+    v.kivesz()
+    v.kivesz()       # most már üres
+    x = v.kivesz()
+    print(x)   
+
+if __name__ == "__main__":
+    main()
