1da1b3f43d92b8e6a179bce941076a610b185500
Hamar Csaba <hamarcsabika@gmail.com>
2026-05-01 07:28:07 +0200

    nyomtatandó oldalak

---
+++ 12/20140314a.py
@@ -0,0 +1,23 @@
+# nyomtatandó oldalak
+
+def nyomtatas(s):
+    oldalak = s.split(',')
+    nyomtatando = []
+    for oldal in oldalak:
+        if '-' in oldal:
+            start, end = oldal.split('-')
+            nyomtatando.extend(range(int(start), int(end)+1))
+        else:
+            nyomtatando.append(int(oldal))
+    return nyomtatando
+
+
+
+
+def main():
+    be = input("Kérem adja meg a nyomtatandó oldalakat pl.: 1-4,7,9,11-15 : ")
+    print(nyomtatas(be))
+
+
+if __name__ == "__main__":
+    main()
